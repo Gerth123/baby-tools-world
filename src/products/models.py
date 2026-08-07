939503364858469849cf851809520ba47894a062
Gerth123 <robingerth21@gmail.com>
2026-08-07 22:13:07 +0200

    Added the optional many-to-many "tags" field to Product model at products/models.py

--- src/products/models.py
+++ src/products/models.py
@@ -45,6 +45,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to="imgs/products/", null=True, blank=True)
     name = models.CharField(max_length=80, blank=False, null=False)
     price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal("0.00"))])
+    tags = models.ManyToManyField(Tag, blank=True, related_name="products")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
